02779aecbf94f56c4e945a8f21405b4173620dc5
cmgcmg <cmg321@naver.com>
2021-03-04 13:49:27 +0900

    first commit

---
+++ Java_1/src/Java_1.java
@@ -0,0 +1,12 @@
+
+public class Java_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hi1");
+		System.out.println("hi2");
+		System.out.println("hi3");	
+	}
+
+}
